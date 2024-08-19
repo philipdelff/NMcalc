040b18134b94d97f6134a20a7b8d15798e9449a4
Philip Delff <philip@delff.dk>
2024-08-18 21:36:08 -0400

    adding CVlnorm

--- R/CVlnorm.R
+++ R/CVlnorm.R
@@ -1,3 +1,9 @@
+##' Convert a Variance of a normal distribution to a CV of a
+##' log-normal distribution
+##'
+##' @param omega A variance of a normal distribution
+##' @export
+
 CVlnorm <- function(omega){
     sqrt(exp(omega)-1)
 }
